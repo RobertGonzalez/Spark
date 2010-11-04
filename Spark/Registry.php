ffa0ca9d5927da622ef2508e28f8f43c73e5a1f9
Robert Gonzalez <robert@robert-gonzalez.com>
2010-11-04 15:17:42 -0700

    Added the registry object

---
+++ Spark/Registry.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * Simple PHP Application Release Kit
+ * 
+ * @category Spark
+ * @package Spark
+ * @author Robert Gonzalez <robert@robert-gonzalez.com>
+ */
+/**
+ * Registry object handles warm data storage per request 
+ * 
+ * Typical Registry object pattern, simply acts as a data store for the request
+ * 
+ * @category Spark
+ * @package Spark
+ * @author Robert Gonzalez <robert@robert-gonzalez.com>
+ * @version @package_version@
+ */
+class Spark_Registry {
+    /**
+     * Holds the registry labels and values data
+     * 
+     * @access protected
+     * @var array
+     */                        
+    protected $_register = array();
+    
+    /**
+     * Singleton instance holder
+     * 
+     * @access private
+     * @var Lib_Registry
+     */
+    private static $_instance = null;
+    
+    /**
+     * Make this a private unextendable function so as to enforce singleton
+     * 
+     * @access private
+     */
+    final private function __construct() {}
+    
+    /**
+     * Set up the singleton instance getter method
+     *
+     * This has to be static (else how would we ever get it?)
+     * 
+     * @access public
+     * @return Lib_Registry
+     */
+    public static function getInstance() {
+        if (self::$_instance === null) {
+            self::$_instance = new self;
+        }
+        
+        return self::$_instance;
+    }
+    
+    /**
+     * Sets a value into the registry if there is no value with this label already
+     * 
+     * @access public
+     * @param string $label The name of the registry entry
+     * @param mixed $value The value to apply to this label     
+     */                        
+    public function set($label, $value) {
+        if (!$this->has($label)) {
+            $this->_register[$label] = $value;
+        }
+    }
+    
+    /**
+     * Magic setter method to allow direct assignment of values
+     * 
+     * @access public
+     * @param string $label The name of the registry entry
+     * @param mixed $value The value to apply to this label     
+     */                        
+    public function __set($label, $value) {
+        $this->set($label, $value);
+    }
+    
+    /**
+     * Gets a value from the registry if there is a label with this value
+     * 
+     * @access public
+     * @param string $label The name of the registry entry
+     * @param mixed $default Default value to send back if the requested label is not found
+     * @return mixed The value that is assigned to this label
+     */                        
+    public function get($label, $default = null) {
+        return $this->has($label) ? $this->_register[$label] : $default;
+    }
+    
+    /**
+     * Magic getter that fetches a registered value directly as a property
+     * 
+     * @access public
+     * @param string $label The name of the registry entry
+     * @param mixed $default Default value to send back if the requested label is not found
+     * @return mixed The value that is assigned to this label
+     */                        
+    public function __get($label) {
+        return $this->get($label);
+    }
+    
+    /**
+     * Checks the existence of an entry in the register
+     * 
+     * Normally you would want to just check isset(), but since isset returns 
+     * false for null values and there may be times when you want to register a
+     * null value you are better off checking the if the key exists which will 
+     * return true even for null values of an index.
+     * 
+     * @access public
+     * @param string $label The name of the registry entry
+     * @return boolean True if the label is in the register
+     */
+    public function has($label) {
+        return array_key_exists($label, $this->_register);
+    }
+    
+    /**
+     * Magic overloader check method to see if a label exists
+     * 
+     * @access public
+     * @param string $label The name of the registry entry
+     * @return boolean True if the label is in the register
+     */
+    public function __isset($label) {
+        return $this->has($label);
+    }
+}
